0820ce4650c4459f6382b05a132d8d209c00b669
Eli Cusic <tntitans31@gmail.com>
2021-02-27 14:30:19 -0600

    set up migrations in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# DB setup for v2 trial - kitchen supply management system
+
+# rails g resource User name:string
+# rails g resource Pantry user:belongs_to => has_many :consumables
+# rails g resource Recipe user:belongs_to name:string instructions:text blog:text => has_many :ingredients
+
+# rails g resource Consumable pantry:belongs_to name:string cost:float unit:string quantity:float category:string store:string => belongs_to Panry??
+# rails g resource Ingredient recipe:belongs_to consumable_id:integer quantity:float => will this overpopulate DB???
+# rails g resource FoodOrder => user:belongs_to => contains an array constant of Recipes with corresponding amount - should I make a separate Recipe model to belong_to FoodOrder???
+
