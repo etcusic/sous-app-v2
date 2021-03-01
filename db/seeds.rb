8ee6528ca866da1b83c2f4c7bf88bd2b3e6bb005
Eli Cusic <tntitans31@gmail.com>
2021-03-01 14:51:21 -0600

    outlined migrations in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
-# DB setup for v2 trial - kitchen supply management system
+# rails g scaffold User name:string
+# rails g scaffold Pantry user:belongs_to => has_many :consumables
+# rails g scaffold Recipe user:belongs_to name:string instructions:text => has_many :ingredients
 
-# rails g resource User name:string
-# rails g resource Pantry user:belongs_to => has_many :consumables
-# rails g resource Recipe user:belongs_to name:string instructions:text blog:text => has_many :ingredients
+# rails g scaffold Consumable pantry:belongs_to name:string cost:float unit:string quantity:float category:string store:string => belongs_to Panry??
+# rails g scaffold Ingredient recipe:belongs_to consumable_id:integer quantity:float => will this overpopulate DB???
 
-# rails g resource Consumable pantry:belongs_to name:string cost:float unit:string quantity:float category:string store:string => belongs_to Panry??
-# rails g resource Ingredient recipe:belongs_to consumable_id:integer quantity:float => will this overpopulate DB???
+# add has_many to Recipe & Pantry & Consumable
 # rails g resource FoodOrder => user:belongs_to => contains an array constant of Recipes with corresponding amount - should I make a separate Recipe model to belong_to FoodOrder???
-
